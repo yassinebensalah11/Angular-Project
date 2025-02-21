1cecfbc6cda3cb609bc14b769778ed7ca4ff9804
yassinebensalah11 <bensalahyassine56@gmail.com>
2025-02-21 01:00:44 +0100

    button next

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ResidencesComponent } from './residences/residences.component';
 import { HomeComponent } from './home/home.component';
@@ -6,11 +6,11 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { ResidenceDetailsComponent } from './residences/residence-details/residence-details.component';
 
 const routes: Routes = [
-{path:'',redirectTo:'/home',pathMatch:'full'},
-{path:'residence',component:ResidencesComponent},
-{path:'home',component:HomeComponent},
-{path:'details/:id',component:ResidenceDetailsComponent}, 
-{path:'**',component:NotFoundComponent}
+  {path:'',redirectTo:'/home',pathMatch:'full'},
+  {path:'residence',component:ResidencesComponent},
+  {path:'home',component:HomeComponent},
+  {path:'details/:id',component:ResidenceDetailsComponent}, 
+  {path:'**',component:NotFoundComponent}
 ];
 
 @NgModule({
--- src/app/residences/residence-details/residence-details.component.ts
+++ src/app/residences/residence-details/residence-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
+import { Residence } from 'src/core/models/Residence';
 
 @Component({
   selector: 'app-residence-details',
@@ -8,14 +9,30 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class ResidenceDetailsComponent implements OnInit {
   id!: number;
-  constructor(private ar: ActivatedRoute) {}
+  residence!: Residence | undefined;
+    listResidences: Residence[] = [
+      { id: 1, name: "El fel", address: "Borj Cedria", image: "../../assets/img/1.jpg", status: "Disponible" },
+      { id: 2, name: "El yasmine", address: "Ezzahra", image: "../../assets/img/2.jpg", status: "Disponible" },
+      { id: 3, name: "El Arij", address: "Rades", image: "../../assets/img/3.jpg", status: "Vendu" },
+      { id: 4, name: "El Anber", address: "inconnu", image: "../../assets/img/4.jpg", status: "En Construction" }
+    ];
+  constructor(private ar: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
     //this.id = this.ar.snapshot.params['id'];
     //console.log(this.id);
     this.ar.params.subscribe(params => {
-      this.id = params['id'];
+      this.id = +params['id']; // Conversion en nombre
+      this.residence = this.listResidences.find(res => res.id === this.id);
       console.log(this.id);
     });
+    }
+
+    nextResidence() {
+      const currentIndex = this.listResidences.findIndex(res => res.id === this.id);
+      if (currentIndex !== -1 && currentIndex < this.listResidences.length - 1) {
+        const nextId = this.listResidences[currentIndex + 1].id;
+        this.router.navigate(['/details', nextId]);
+      }
     }
    }
